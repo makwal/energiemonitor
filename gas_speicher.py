c3a3a7f884a18bf396557ea5f6fdf641f578064c
Mark <ma-wa@gmx.ch>
2022-12-28 14:31:01 +0000

    auto-update

--- gas_speicher.py
+++ gas_speicher.py
@@ -14,31 +14,32 @@ from energy_settings import (
     backdate,
     datawrapper_api_key,
     datawrapper_url,
-    datawrapper_headers
+    datawrapper_headers,
+    curr_year
 )
 import locale
 locale.setlocale(locale.LC_TIME, 'de_CH.UTF-8')
 
 
-# In[2]:
+# In[ ]:
 
 
 headers = {'x-key': api_key_agsi}
 
 
-# In[3]:
+# In[ ]:
 
 
 countries = ['de', 'fr']
 
 
-# In[4]:
+# In[ ]:
 
 
 url = 'https://agsi.gie.eu/api?type=eu&country={}&size=300&page={}'
 
 
-# In[5]:
+# In[ ]:
 
 
 def data_requester(country, num):
@@ -50,7 +51,7 @@ def data_requester(country, num):
         return f'Fehler beim request: {r.status_code}'
 
 
-# In[6]:
+# In[ ]:
 
 
 def data_preparator(country_code):
@@ -72,7 +73,7 @@ def data_preparator(country_code):
     return df_country
 
 
-# In[7]:
+# In[ ]:
 
 
 def data_finalizer(country_code):
@@ -88,45 +89,48 @@ def data_finalizer(country_code):
     df.reset_index(drop=True, inplace=True)
 
     df['full'] = df['full'].astype(float)
-    
+
     #Zur Kontrolle, ob die Werte nicht zu stark vom Vortag abweichen
-    
+
     df['full_control_diff'] = df['full'].diff()
-    
+
     max_diff = df['full_control_diff'].max() * 1.25
     min_diff = df['full_control_diff'].min() * 1.5
-    
+
     last_diff = df['full_control_diff'].tail(1).values[0]
 
     df_mean = df[df['gasDayStart'] <= '2021-12-31'].groupby('date_datawrapper')['full'].mean().to_frame()
     df_max = df[df['gasDayStart'] <= '2021-12-31'].groupby('date_datawrapper')['full'].max().to_frame()
     df_min = df[df['gasDayStart'] <= '2021-12-31'].groupby('date_datawrapper')['full'].min().to_frame()
 
-    df22 = df[df['gasDayStart'] >= '2022-01-01'].copy()
+    #Ein Dataframe für die Jahre seit 2022
+    df_curr = df[df['year'] >= 2022].pivot(index='date_datawrapper', columns='year', values='full')
 
     df_mean.rename(columns={'full': 'Mittelwert'}, inplace=True)
     df_max.rename(columns={'full': 'Maximum'}, inplace=True)
     df_min.rename(columns={'full': 'Minimum'}, inplace=True)
-    df22.rename(columns={'full': '2022'}, inplace=True)
-
-    df22.set_index('date_datawrapper', inplace=True)
-    df22.sort_index(inplace=True)
-
-    df_end = df22[['2022']].join([df_mean, df_max, df_min], how='outer')
-
-    df_end.sort_index(inplace=True)
-    df_end.reset_index(inplace=True)
     
-    df_end.columns = ['date', '2022', 'Mittelwert', 'Maximum', 'Minimum']
-        
-    df_end = df_end[df_end['date'] != '2022-02-29'].copy()
-        
+    df_end = df_curr.join([df_mean, df_max, df_min], how='outer')
+
+    df_end.reset_index(inplace=True)
+
+    df_end = df_end[df_end['date_datawrapper'] != '2022-02-29'].copy()
+
+    all_columns = ['date_datawrapper']
+
+    curr_columns = df_curr.columns.tolist()
+    curr_columns.sort(reverse=True)
+    all_columns.extend(curr_columns)
+    all_columns.extend(['Mittelwert', 'Maximum', 'Minimum'])
+
+    df_end = df_end[all_columns].copy()
+
     return df_end, max_diff, min_diff, last_diff
 
 
 # **Datawrapper-Update**
 
-# In[8]:
+# In[ ]:
 
 
 chart_ids = {
@@ -135,10 +139,10 @@ chart_ids = {
 }
 
 
-# In[9]:
+# In[ ]:
 
 
-def chart_updater(chart_id, title, last_updated):
+def chart_updater(chart_id, title, note):
 
     url_update = datawrapper_url + chart_id
     url_publish = url_update + '/publish'
@@ -146,7 +150,7 @@ def chart_updater(chart_id, title, last_updated):
     payload = {
 
         'title': title,
-        'metadata': {'annotate': {'notes': f'Wird täglich aktualisiert. Datenstand: {last_updated}.'}}
+        'metadata': {'annotate': {'notes': note}}
 
     }
 
@@ -161,17 +165,19 @@ def chart_updater(chart_id, title, last_updated):
 
 # Die Grafik wird nur aktualisiert, wenn der letzte Wert nicht grösser ist als das Maximum der ganzen Reihe resp. nicht kleiner als das doppelte Minimum der Reihe. Damit soll verhindert werden, dass falsche Daten in die Grafik drücken.
 
-# In[10]:
+# In[ ]:
 
 
 for country, chart_id in chart_ids.items():
     
     df_export, max_diff, min_diff, last_diff = data_finalizer(country)
     
-    last_updated = df_export[df_export['2022'].notna()]['date'].tail(1).values[0]
+    last_updated = df_export[df_export[curr_year].notna()]['date'].tail(1).values[0]
     last_updated = pd.to_datetime(last_updated).strftime('%-d. %B %Y')
     
-    latest_value = df_export[df_export['2022'].notna()]['2022'].tail(1).values[0].round(1)
+    note = f'''    Mittelwert, Maximum und Minimum der Jahre 2011 bis 2021.     Wird täglich aktualisiert. Datenstand: {last_updated}.    '''
+    
+    latest_value = df_export[df_export[curr_year].notna()][curr_year].tail(1).values[0].round(1)
     latest_value = str(latest_value).replace('.', ',')
     
     if country == 'de':
@@ -188,7 +194,7 @@ for country, chart_id in chart_ids.items():
         #Data
         df_export.to_csv(f'/root/energiemonitor/data/gas/gas_speicher_{country}.csv', index=False)
         
-        chart_updater(chart_id, chart_title, last_updated)
+        chart_updater(chart_id, chart_title, note)
     else:
         print('Die aktuellen Daten scheinen fehlerhaft zu sein.')
 
